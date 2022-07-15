7b7919b2bb87608a9b56c6c5e70607dd321c2e60
hayato-kame-new <guishan.meisha0330@gmail.com>
2022-07-15 14:09:37 +0900

    methodSmall methodBig

--- app/src/main/java/to/msn/wings/sevengame/Judgement.kt
+++ app/src/main/java/to/msn/wings/sevengame/Judgement.kt
@@ -43,7 +43,7 @@ class Judgement( poList: ArrayList<PossibleCard> ){
                 }
             }
             if (count == 6) {
-                // 13まで置かれているから 1を置いてる つまり逆向きになってる
+                // カウント6だったら、13まで置かれていて 1を置いてる つまり逆向きになってる
                 //  1 2 3 4 5 6 と置けるようにする
                 for (num in 1..6) {
                     var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
@@ -53,11 +53,10 @@ class Judgement( poList: ArrayList<PossibleCard> ){
                     }
                 }
             } else {
-                // そうでなければ、普通の向きです
-                // 今度は 13から置くように変更する
+                // カウント6 でなければ、普通の向きです "1"を置いたから 次は  13から置くように変更する
                 for (num in 13 downTo 8) {
                     var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
-                    // 余計な possibleをクリアする
+                    // 余計な possibleをクリアする 一旦全部クリアする 8 ~ 13 の
                     if (card != null && card.placed == false && card.possible == true) {
                         card.possible = false //全部　置ける を 一旦 置けない でクリアしておく 全部だから breakはしない
                     }
@@ -72,106 +71,71 @@ class Judgement( poList: ArrayList<PossibleCard> ){
                     }
                 }
             }
-        }
-
-
-        if (putCardNum in 2..6) {
-            var count = 0
-            for (num in 8..13) {
+        } else {
+            for (num in 6 downTo 1) {
                 var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
-                if (card != null && card.placed == true) {
-                    count++
-                }
-            }
-            if (count == 6) {
-                // 13まで置かれているから 1を置いてる つまり逆向きになってる
-                //  1 2 3 4 5 6 と置けるようにする
-                for (num in 1..6) {
-                    var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
-                    if (card != null && card.placed == false) { // もし、5から１まで　リストに まだ置いてないカードが見つかった時点で
-                        card.possible = true // 可能に trueを入れる 見つかった時点ですぐbreak
-                        break // 抜ける
-                    }
-                }
-            } else {
-                // そうでなければ、普通の向きです
-                // 今度は 13から置くように変更する
-                for (num in 13 downTo 8) {
-                    var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
-                    // 余計な possibleをクリアする
-                    if (card != null && card.placed == false && card.possible == true) {
-                        card.possible = false //全部　置ける を 一旦 置けない でクリアしておく 全部だから breakはしない
-                    }
-                }
-                // もう一度別のループで設定し直す
-                for (num in 13 downTo 8) {
-                    var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
-                    // 設定し直しする
-                    if (card != null && card.placed == false) { // もし、まだ置いてないカードが見つかった時点で
-                        card.possible = true // 可能に trueを入れる 最初に見つかったやつだけ
-                        break // 抜ける 最初に見つかったら trueにしたらすぐループを抜ける
-                    }
+                if (card != null && card.placed == false) { // もし、13から8まで　リストに まだ置いてないカードが見つかった時点で
+                    card.possible = true // 可能に trueを入れる 見つかった時点ですぐbreak
+                    break // 抜ける
                 }
             }
         }
         return _deepPossibleCardList  // 最後書き換えたのを返す 呼び出し元で必要です
     }
-    // メソッドここまで
+
 
     /**
      * 置いたカード大きい時
      */
     fun methodBig(pTagStr: String): ArrayList<PossibleCard> {
         val putCardNum = pTagStr.substring(1).toInt()
-       // if (putCardNum == 13) {
-            var count = 0
-            for (num in 1..6) {
-                var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
-                if (card != null && card.placed == true) {
-                    count++
+            if (putCardNum == 13) {
+                var count = 0
+                for (num in 1..6) {
+                    var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
+                    if (card != null && card.placed == true) {
+                        count++
+                    }
                 }
-            }
-            if (count == 6) {
-                // 1まで置かれているから 13を置いてる つまり逆向きになってる
-                //  13 12 11 10 9 8  と置けるようにする
-                for (num in 13 downTo 8) {
+                if (count == 6) {
+                    // カウントが 6ならば 1まで置かれていて 13を置いてる つまり逆向きになってる
+                    //  13 12 11 10 9 8  と置けるようにする
+                    for (num in 13 downTo 8) {
+                        var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
+                        if (card != null && card.placed == false) { // もし、13から8まで　リストに まだ置いてないカードが見つかった時点で
+                            card.possible = true // 可能に trueを入れる 見つかった時点ですぐbreak
+                            break // 抜ける
+                        }
+                    }
+                } else {
+                    // カウントが 6 でなければ、普通の向きです
+                    // 今度は 1から置くように変更する
+                    for (num in 1..6) {
+                        var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
+                        // 余計な possibleをクリアする
+                        if (card != null && card.placed == false && card.possible == true) {
+                            card.possible = false //全部　置ける を 一旦 置けない でクリアしておく 全部だから breakはしない
+                        }
+                    }
+                    // もう一度別のループで設定し直す
+                    for (num in 1..6) {
+                        var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
+                        // 設定し直しする
+                        if (card != null && card.placed == false) { // もし、まだ置いてないカードが見つかった時点で
+                            card.possible = true // 可能に trueを入れる 最初に見つかったやつだけ
+                            break // 抜ける 最初に見つかったら trueにしたらすぐループを抜ける
+                        }
+                    }
+                }
+            } else {
+                for (num in 8..13) {
                     var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
                     if (card != null && card.placed == false) { // もし、13から8まで　リストに まだ置いてないカードが見つかった時点で
                         card.possible = true // 可能に trueを入れる 見つかった時点ですぐbreak
                         break // 抜ける
                     }
                 }
-            } else {
-                // カウントが 6 でなければ、普通の向きです
-                // 今度は 1から置くように変更する
-                for (num in 1..6) {
-                    var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
-                    // 余計な possibleをクリアする
-                    if (card != null && card.placed == false && card.possible == true) {
-                        card.possible = false //全部　置ける を 一旦 置けない でクリアしておく 全部だから breakはしない
-                    }
-                }
-                // もう一度別のループで設定し直す
-                for (num in 1..6) {
-                    var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
-                    // 設定し直しする
-                    if (card != null && card.placed == false) { // もし、まだ置いてないカードが見つかった時点で
-                        card.possible = true // 可能に trueを入れる 最初に見つかったやつだけ
-                        break // 抜ける 最初に見つかったら trueにしたらすぐループを抜ける
-                    }
-                }
             }
-       // }
-
-//        if (putCardNum in 2..6) {
-//            for (num in 5 downTo 1) {  // 属性を書き換えるだけだから forが使える
-//                var card = _game.getPossibleCard(_deepPossibleCardList, pTagStr, num)
-//                if (card != null && card.placed == false) { // もし、5から１まで　リストに まだ置いてないカードが見つかった時点で
-//                    card.possible = true // 可能に trueを入れる 見つかった時点ですぐbreak
-//                    break // 抜ける
-//                }
-//            }
-//        }
         return _deepPossibleCardList  // 最後書き換えたのを返す 呼び出し元で必要です
     }
 }
